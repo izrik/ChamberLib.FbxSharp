e8cc160445731c9ea1b9554f02cd87f7a9a5d9a7
izrik <izrik@izrik.com>
2015-02-27 10:31:43 -0600

    Initial project files.

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("ChamberLib.FbxSharp")]
+[assembly: AssemblyDescription("Load models from FBX files using FbxSharp")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Metaphysics Industries, Inc.")]
+[assembly: AssemblyProduct("")]
+[assembly: AssemblyCopyright("Copyright 2015 izrik and Metaphysics Industries, Inc.")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: AssemblyVersion("0.1")]
