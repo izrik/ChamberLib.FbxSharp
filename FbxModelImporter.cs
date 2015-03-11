47fb8bf4c144f7412434c72e03450623262e47e1
izrik <izrik@izrik.com>
2015-03-11 09:45:45 -0500

    Initial form of the importer. It doesn't import anything yet.

---
+++ FbxModelImporter.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using ChamberLib.Content;
+using System.IO;
+using FbxSharp;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ChamberLib.FbxSharp
+{
+    public class FbxModelImporter
+    {
+        public FbxModelImporter(ModelImporter next=null)
+        {
+            this.next = next;
+        }
+
+        readonly ModelImporter next;
+
+        public ModelContent ImportModel(string filename, IContentImporter importer)
+        {
+            if (File.Exists(filename))
+            {
+            }
+            else if (File.Exists(filename + ".fbx"))
+            {
+                filename += ".fbx";
+            }
+            else if (next != null)
+            {
+                return next(filename, importer);
+            }
+            else
+            {
+                throw new FileNotFoundException("The file could not be found", filename);
+            }
+
+            var fimporter = new Importer();
+
+            var scene = fimporter.Import(filename);
+
+
+
+            var model = new ModelContent();
+            model.Filename = filename;
+            return model;
+        }
+    }
+}
+
