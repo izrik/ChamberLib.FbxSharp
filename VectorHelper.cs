a187afa469eca8277d14740dc117224214508613
izrik <izrik@izrik.com>
2015-03-13 09:17:53 -0500

    Convert from FbxSharp vectors to ChamberLib's.

---
+++ VectorHelper.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ChamberLib.FbxSharp
+{
+    public static class VectorHelper
+    {
+        public static ChamberLib.Vector2 ToChamber(this global::FbxSharp.Vector2 v)
+        {
+            return
+                new Vector2(
+                    (float)v.X,
+                    (float)v.Y);
+        }
+
+        public static ChamberLib.Vector3 ToChamber(this global::FbxSharp.Vector3 v)
+        {
+            return
+                new Vector3(
+                    (float)v.X,
+                    (float)v.Y,
+                    (float)v.Z);
+        }
+
+        public static ChamberLib.Vector4 ToChamber(this global::FbxSharp.Vector4 v)
+        {
+            return
+                new Vector4(
+                    (float)v.X,
+                    (float)v.Y,
+                    (float)v.Z,
+                    (float)v.W);
+        }
+    }
+}
+
